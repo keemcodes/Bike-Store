766322f2c7eba9e6f8041a353751fb966f3faebb
Keem Codes <akeeminbox@gmail.com>
2022-02-02 20:47:38 -0500

    adding server related changes

--- src/App.js
+++ src/App.js
@@ -12,16 +12,16 @@ export default function App() {
   }
   return (
     <div className="App">
-      <Router>
+      <Router basename={'/'}>
         <nav>
-          <NavLink text="Store" linkTo="/" />
-          <NavLink text="Cart" linkTo="/cart" />
+          <NavLink text="Store" linkTo={`${process.env.PUBLIC_URL}/`} />
+          <NavLink text="Cart" linkTo={`${process.env.PUBLIC_URL}/cart`} />
         </nav>
         <Switch>
-          <Route exact path="/">
+          <Route exact path={`${process.env.PUBLIC_URL}/`}>
             <Store cart={cart} addToCart={addToCart} />
           </Route>
-          <Route exact path="/cart">
+          <Route exact path={`${process.env.PUBLIC_URL}/cart`}>
             <Cart cart={cart} addToCart={addToCart} />
           </Route>
         </Switch>
